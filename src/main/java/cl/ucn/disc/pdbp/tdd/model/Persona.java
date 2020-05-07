f91173a222fa6595a27bb8c84e293dddaef74612
Javier Palacios <jp.palacios@outlook.com>
2020-05-07 17:22:01 -0400

    feat: add of Persona class

---
+++ src/main/java/cl/ucn/disc/pdbp/tdd/model/Persona.java
@@ -0,0 +1,49 @@
+package cl.ucn.disc.pdbp.tdd.model;
+
+public class Persona {
+  private String nombre;
+  private String direccion;
+  private long telefonoFijo;
+  private long telefonoMovil;
+  private String correo;
+  private String rut;
+
+  public Persona(String nombre, String direccion, long telefonoFijo, long telefonoMovil, String correo, String rut) {
+    if (nombre == null || rut == null || (telefonoMovil == 0 && telefonoFijo == 0)) {
+      throw new NullPointerException("La persona debe otorgar al menos un nombre, un telefono y un rut");
+    }
+    if (nombre.length() < 2){
+      throw new RuntimeException("El nombre debe tener al menos 2 letras");
+    }
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.telefonoFijo = telefonoFijo;
+    this.telefonoMovil = telefonoMovil;
+    this.correo = correo;
+    this.rut = rut;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public String getDireccion() {
+    return direccion;
+  }
+
+  public long getTelefonoFijo() {
+    return telefonoFijo;
+  }
+
+  public long getTelefonoMovil() {
+    return telefonoMovil;
+  }
+
+  public String getCorreo() {
+    return correo;
+  }
+
+  public String getRut() {
+    return rut;
+  }
+}
