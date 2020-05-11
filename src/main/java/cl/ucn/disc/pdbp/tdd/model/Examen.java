2adba797d86ce5ba4c161a46eefa27508c94825f
Javier Palacios <jp.palacios@outlook.com>
2020-05-11 16:35:56 -0400

    doc: examen class documentation

--- src/main/java/cl/ucn/disc/pdbp/tdd/model/Examen.java
+++ src/main/java/cl/ucn/disc/pdbp/tdd/model/Examen.java
@@ -2,25 +2,53 @@ package cl.ucn.disc.pdbp.tdd.model;
 
 import java.time.LocalDateTime;
 
+/**
+ * examen completado a una mascota
+ * @author Javier Palacios
+ */
 public class Examen {
+  /**
+   * nombre del examen
+   */
   private String nombre;
+  /**
+   * resultado del examen
+   */
   private String resultado;
+  /**
+   * fecha en la que se realizo en examen
+   */
   private LocalDateTime fecha;
 
+  /**
+   * constructor del examen
+   * @param nombre
+   * @param resultado
+   * @param fecha
+   */
   public Examen(String nombre, String resultado, LocalDateTime fecha) {
     this.nombre = nombre;
     this.resultado = resultado;
     this.fecha = fecha;
   }
 
+  /**
+   * @return nombre
+   */
   public String getNombre() {
     return nombre;
   }
 
+  /**
+   * @return resultado
+   */
   public String getResultado() {
     return resultado;
   }
 
+  /**
+   * @return fecha
+   */
   public LocalDateTime getFecha() {
     return fecha;
   }
