bd0cd2ec3f8ffed77302d7db06b9e3dfedc6bd9f
Javier Palacios <jp.palacios@outlook.com>
2020-05-11 17:01:22 -0400

    doc: control class documentation

--- src/main/java/cl/ucn/disc/pdbp/tdd/model/Control.java
+++ src/main/java/cl/ucn/disc/pdbp/tdd/model/Control.java
@@ -2,15 +2,50 @@ package cl.ucn.disc.pdbp.tdd.model;
 
 import java.time.LocalDateTime;
 
+/**
+ * control realizado por una mascota
+ * @autor Javier Palacios
+ */
 public class Control {
+  /**
+   * fecha en que se realizo el control
+   */
   private LocalDateTime fecha;
+  /**
+   * fecha en la que debe realizar el proximo control
+   */
   private LocalDateTime proximoControl;
+  /**
+   * temperatura que registro la mascota
+   */
   private float temperatura;
+  /**
+   * peso que registro la mascota
+   */
   private float peso;
+  /**
+   * altura que registro la mascota
+   */
   private float altura;
+  /**
+   * diagnostico para la mascota
+   */
   private String diagnostico;
+  /**
+   * veterinario que atendio a la mascota
+   */
   private String veterinario;
 
+  /**
+   * constructor del control
+   * @param fecha
+   * @param proximoControl
+   * @param temperatura
+   * @param peso
+   * @param altura
+   * @param diagnostico
+   * @param veterinario
+   */
   public Control(LocalDateTime fecha, LocalDateTime proximoControl, float temperatura, float peso, float altura,
                  String diagnostico, String veterinario) {
     this.fecha = fecha;
@@ -22,30 +57,51 @@ public class Control {
     this.veterinario = veterinario;
   }
 
+  /**
+   * @return fecha control
+   */
   public LocalDateTime getFecha() {
     return fecha;
   }
 
+  /**
+   * @return fecha proximo control
+   */
   public LocalDateTime getProximoControl() {
     return proximoControl;
   }
 
+  /**
+   * @return temperatura registrada
+   */
   public float getTemperatura() {
     return temperatura;
   }
 
+  /**
+   * @return peso registrado
+   */
   public float getPeso() {
     return peso;
   }
 
+  /**
+   * @return altura registrada
+   */
   public float getAltura() {
     return altura;
   }
 
+  /**
+   * @return diagnostico recibido
+   */
   public String getDiagnostico() {
     return diagnostico;
   }
 
+  /**
+   * @return veterinario responsable
+   */
   public String getVeterinario() {
     return veterinario;
   }
